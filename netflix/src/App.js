a0c2232b9f017cf4fda4bf6730bd6f81f628168e
Brandon Humphry <brandonhumphry@gmail.com>
2020-10-17 23:17:35 -0700

    installed axios and set up js file to request movie info from TMDB

---
+++ netflix/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* API Key */}
+      {/* a17804ed558823f3a468f7d87c09de92 */}
+    </div>
+  );
+}
+
+export default App;
