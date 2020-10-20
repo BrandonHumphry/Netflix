83393542d93566881099b7eac382a33493c83e37
Brandon Humphry <brandonhumphry@gmail.com>
2020-10-19 17:15:47 -0700

    styling for poster rows

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './App.css';
-import Row from './Row.js';
-import requests from './requests'
+import React from "react";
+import "./App.css";
+import Row from "./Row.js";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
     </div>
   );
 }
--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-import requests from "./requests";
+import "./Row.css";
+
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 // common base url for image in movies object
@@ -27,14 +28,18 @@ function Row({ title, fetchUrl }) {
 
   return (
     <div className="row">
-      {/* title */}
       <h2>{title}</h2>
 
       {/* create a contaner to be able to hold and scroll posters */}
       <div className="row_posters">
         {/* container -> movie posters, map over movies and pull out poster image for each */}
         {movies.map(movie => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
