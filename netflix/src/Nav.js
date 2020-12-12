5c307a7f794dcfedaea1aa0e49da4cf1dceef817
Brandon Humphry <brandonhumphry@gmail.com>
2020-12-11 22:48:13 -0800

    updating nav logo

--- netflix/src/Nav.js
+++ netflix/src/Nav.js
@@ -18,7 +18,7 @@ function Nav() {
     <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo"
       />
       <img
